8c6d596d64931cd7df69ae77e2a1f67cda9552e6
chenyuan <chenyuan@chenyuan-MBP.local>
2014-07-10 18:11:21 +0800

    自己写了一个listbox插件

---
+++ src/main/plugin/listbox/jquery.listbox.js
@@ -0,0 +1,125 @@
+(function ($) {
+
+    ListBoxUtil = function (options) {
+        this.options = options || {};
+        this.target = null;
+        this.targetWrap = null;
+        this.width = 600;
+        this.top = 0;
+        this.left = 0;
+        this.id = "listbox";
+        this.className = "";
+        this.bodyHtml = "";
+        this.currentIndex = 0;
+        var html = [
+                '<div class="ac-renderer" role="listbox" id="' + this.id + '" style="-webkit-user-select: none;">',
+            '</div>'
+        ].join('');
+        this.listbox = $(html);
+    };
+
+    ListBoxUtil.prototype.init = function () {
+        var this$ = this;
+        this$.setConfig();
+        this$.mouseHover();
+        this$.keyup();
+        this$.target.on('focus', function () {
+            this$.show();
+        }).on('blur', function () {
+            this$.hide();
+        });
+    };
+
+    ListBoxUtil.prototype.setConfig = function () {
+        if (this.options != null) {
+            if (this.options['target']) {
+                this.target = this.options['target'];
+            }
+            if (this.options['targetWrap']) {
+                this.targetWrap = this.options['targetWrap'];
+            }
+            if (this.options['width']) {
+                this.listbox.width(this.options['width']);
+            }
+            if (this.options['bodyHtml']) {
+                this.listbox.append(this.options['bodyHtml']);
+            }
+            if (this.options['left']) {
+                this.listbox.left(this.options['left']);
+            } else {
+                this.listbox.css("left", this.target.offset().left);
+            }
+            if (this.options['top']) {
+                this.listbox.top(this.options['top']);
+            } else {
+                this.listbox.css("top", this.target.offset().top + this.target.height() + 10);
+            }
+            if (this.options['className']) {
+                this.listbox.removeClass().addClass(this.options['className']);
+            }
+            this.targetWrap.append(this.listbox);
+        }
+    };
+
+    ListBoxUtil.prototype.mouseHover = function () {
+        var this$ = this;
+        this.listbox.children().hover(
+            function () {
+                this$.listbox.children().removeClass('ac-active active');
+                $(this).addClass("ac-active active");
+            }
+        );
+    };
+
+    ListBoxUtil.prototype.keyMove = function (e) {
+        var listChild = this.listbox.children().removeClass("ac-active active");
+        // 向上
+        if (e.keyCode == 38) {
+            if (this.currentIndex > 0) {
+                this.currentIndex--;
+            } else {
+                this.currentIndex = listChild.length - 1;
+            }
+        }
+        // 向下
+        else if (e.keyCode == 40) {
+            if (this.currentIndex == listChild.length - 1) {
+                this.currentIndex = 0;
+            } else {
+                this.currentIndex++;
+            }
+        }
+        listChild.eq(this.currentIndex).addClass("ac-active active");
+    };
+
+    ListBoxUtil.prototype.hide = function () {
+        this.listbox.hide();
+    };
+
+    ListBoxUtil.prototype.show = function () {
+        this.listbox.show();
+    };
+
+    ListBoxUtil.prototype.bindClick = function () {
+        this.listbox.children().on('click', function () {
+
+        });
+    };
+
+    ListBoxUtil.prototype.enter = function (e) {
+        var curentChild = this.listbox.children().eq(this.currentIndex);
+        console.log(curentChild);
+    };
+
+    ListBoxUtil.prototype.keyup = function () {
+        var this$ = this;
+        this.target.on('keyup', function (e) {
+            if (e.keyCode == 38 || e.keyCode == 40) {
+                this$.keyMove(e);
+            } else if (e.keyCode == 13) {
+                this$.enter(e);
+            }
+        });
+    };
+
+})(jQuery);
